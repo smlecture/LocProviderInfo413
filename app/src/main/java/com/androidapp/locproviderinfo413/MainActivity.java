c36fcb8416451141178764f61810f2ea01d37c07
H-J Yoo <yoohj@smu.ac.kr>
2021-04-13 09:41:44 +0900

    location providers

---
+++ app/src/main/java/com/androidapp/locproviderinfo413/MainActivity.java
@@ -0,0 +1,33 @@
+package com.androidapp.locproviderinfo413;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mTxtInfo;
+    LocationManager locManager;
+    List<String> locProviderList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTxtInfo = findViewById(R.id.txtInfo);
+
+        locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locProviderList = locManager.getAllProviders();
+        mTxtInfo.setText("");
+        for(String locProvider : locProviderList){
+            mTxtInfo.append("Loc. Provider: " + locProvider + "\n");
+            mTxtInfo.append("Enabled: " + locManager.isProviderEnabled(locProvider) + "\n\n"); // Status
+        }
+    }
+}
